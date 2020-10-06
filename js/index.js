8bc008617a8d7b7305a5bf2f264e4424b647f5f8
mushfiqurmahi <mushfiqurmahi2003@gmail.com>
2020-10-06 17:08:18 +0600

    sorting working good

--- js/index.js
+++ js/index.js
@@ -41,6 +41,38 @@ for (let i = 0; i < linkCollapse.length; i++) {
 // ------------------------ nav functionality ends -----------------------------
 // ------------------------ some global funcs  -----------------------------
 let firstBy=function(){function n(n){return n}function t(n){return"string"==typeof n?n.toLowerCase():n}function r(r,e){if(e="number"==typeof e?{direction:e}:e||{},"function"!=typeof r){var u=r;r=function(n){return n[u]?n[u]:""}}if(1===r.length){var i=r,o=e.ignoreCase?t:n;r=function(n,t){return o(i(n))<o(i(t))?-1:o(i(n))>o(i(t))?1:0}}return-1===e.direction?function(n,t){return-r(n,t)}:r}function e(n,t){return n=r(n,t),n.thenBy=u,n}function u(n,t){var u=this;return n=r(n,t),e(function(t,r){return u(t,r)||n(t,r)})}return e}();/*** Copyright 2013 Teun Duynstee Licensed under the Apache License, Version 2.0 ***/
+let createRankList = function(data, header_index){
+    let rankList = data.split('\n').splice(1).map(line=>{
+        let result = line.split(',');
+        // [roll, total, optional, non-optional, name]
+        return [
+            parseInt(result[header_index.indexOf('student_roll')].slice(ROLL_SLICE)), 
+            parseInt(result[header_index.indexOf('term_total')]),
+            result[header_index.indexOf('optionalSub')] == 'biology' ? parseInt(result[header_index.indexOf('biology_1st_mcq')]) : parseInt(result[header_index.indexOf('higher_math_1st_mcq')]),
+            result[header_index.indexOf('optionalSub')] == 'biology' ? parseInt(result[header_index.indexOf('higher_math_1st_mcq')]) : parseInt(result[header_index.indexOf('biology_1st_mcq')]),
+            result[header_index.indexOf('student_name')],
+        ]
+    })
+    let sortingFunc = firstBy(function (arr1, arr2) { return arr1[1] - arr2[1]; }, -1)
+        .thenBy(function (arr1, arr2) { return arr1[2] - arr2[2]; },)
+        .thenBy(function (arr1, arr2) { return arr1[3] - arr2[3]; }, -1);
+    //1. who has the highest mark (desc)
+    //2. if marks same? who has low in optional_sub  (asc)
+    //3. if both marks same? who has high in non_optional_sub (desc)
+    
+    rankList.sort(sortingFunc);
+
+    console.log('[RANK]')
+    console.log(rankList)
+
+    let objStoreRank = getObjectStore(db, OBJ_STORE_RANK, 'readwrite');
+    rankList.forEach((arr, index)=>{
+        objStoreRank.add({
+            rank: index+1,
+            data : arr
+        })
+    })
+}
 // ------------------------ some global funcs ends -----------------------------
 // ------------------------ main fetching starts ------------------------------
 // for overview
@@ -54,6 +86,7 @@ const OBJ_STORE_MAIN = `${DB_NAME}_${XM_NAME}_main`;
 const OBJ_STORE_RANK = `${DB_NAME}_${XM_NAME}_rank`;
 const OBJ_STORE_INFO = `${DB_NAME}_${XM_NAME}_info`;
 const DB_VERSION = 1;
+const ROLL_SLICE = 10;
 
 let db;
 
@@ -80,12 +113,15 @@ async function onsuccess_func(event) {
                 .then(res => res.text())
                 .then(data => {
 
+                    // getting header index
+                    let header_index = data.split('\n')[0].split(',');
+
                     // storing main data
                     let objStoreMain = getObjectStore(db, OBJ_STORE_MAIN, 'readwrite');
                     data.split('\n').splice(1).forEach(line => { // header row not needed
                         let result = line.split(',')
                         objStoreMain.add({
-                            roll: parseInt(result[1].slice(10)),
+                            roll: parseInt(result[1].slice(ROLL_SLICE)),
                             name: result[0],
                             result: result.splice(2) // 1st 2 items not needed.
                         })
@@ -93,6 +129,7 @@ async function onsuccess_func(event) {
 
                     // calculating rank
                     
+
                 })
         }
     }
@@ -157,5 +194,8 @@ function openDb(idb, db_name, db_version, onupgradeneeded_func, onsuccess_func) 
 
 function getObjectStore(db, store_name, mode) {
     var tx = db.transaction([store_name], mode);
+    tx.oncomplete = function(e){
+        console.log('[TX CPM :',db,store_name,mode,']');
+    }
     return tx.objectStore(store_name);
 }
